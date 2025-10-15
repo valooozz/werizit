a5bee3ebe0081aeab09fe3df4cc9cb5237946ade
valooozz <valentin.corazza@ikmail.com>
2025-10-15 16:31:51 +0200

    dao

---
+++ lib/data/db/dao.dart
@@ -0,0 +1,75 @@
+import 'database_helper.dart';
+import '../models/house.dart';
+import '../models/room.dart';
+import '../models/furniture.dart';
+import '../models/shelf.dart';
+import '../models/item.dart';
+
+class DAO {
+  final dbHelper = DatabaseHelper.instance;
+
+  // ---------- HOUSE ----------
+  Future<int> insertHouse(House house) async =>
+      await dbHelper.insert('House', house.toMap());
+
+  Future<List<House>> getHouses() async {
+    final maps = await dbHelper.queryAll('House');
+    return maps.map((m) => House.fromMap(m)).toList();
+  }
+
+  // ---------- ROOM ----------
+  Future<int> insertRoom(Room room) async =>
+      await dbHelper.insert('Room', room.toMap());
+
+  Future<List<Room>> getRoomsByHouse(int houseId) async {
+    final db = await dbHelper.database;
+    final maps = await db.query(
+      'Room',
+      where: 'house = ?',
+      whereArgs: [houseId],
+    );
+    return maps.map((m) => Room.fromMap(m)).toList();
+  }
+
+  // ---------- FURNITURE ----------
+  Future<int> insertFurniture(Furniture f) async =>
+      await dbHelper.insert('Furniture', f.toMap());
+
+  Future<List<Furniture>> getFurnitureByRoom(int roomId) async {
+    final db = await dbHelper.database;
+    final maps = await db.query(
+      'Furniture',
+      where: 'room = ?',
+      whereArgs: [roomId],
+    );
+    return maps.map((m) => Furniture.fromMap(m)).toList();
+  }
+
+  // ---------- SHELF ----------
+  Future<int> insertShelf(Shelf shelf) async =>
+      await dbHelper.insert('Shelf', shelf.toMap());
+
+  Future<List<Shelf>> getShelvesByFurniture(int furnitureId) async {
+    final db = await dbHelper.database;
+    final maps = await db.query(
+      'Shelf',
+      where: 'furniture = ?',
+      whereArgs: [furnitureId],
+    );
+    return maps.map((m) => Shelf.fromMap(m)).toList();
+  }
+
+  // ---------- ITEM ----------
+  Future<int> insertObject(Item obj) async =>
+      await dbHelper.insert('ObjectItem', obj.toMap());
+
+  Future<List<Item>> getObjectsByShelf(int shelfId) async {
+    final db = await dbHelper.database;
+    final maps = await db.query(
+      'ObjectItem',
+      where: 'shelf = ?',
+      whereArgs: [shelfId],
+    );
+    return maps.map((m) => Item.fromMap(m)).toList();
+  }
+}
---
+++ lib/data/db/database_helper.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "storage.db";
+  static const _databaseVersion = 1;
+
+  static final DatabaseHelper instance = DatabaseHelper._internal();
+  DatabaseHelper._internal();
+
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, _databaseName);
+
+    return openDatabase(path, version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE House(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE Room(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        house INTEGER,
+        FOREIGN KEY (house) REFERENCES House (id) ON DELETE CASCADE
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE Furniture(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        room INTEGER,
+        FOREIGN KEY (room) REFERENCES Room (id) ON DELETE CASCADE
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE Shelf(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        furniture INTEGER,
+        FOREIGN KEY (furniture) REFERENCES Furniture (id) ON DELETE CASCADE
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE ObjectItem(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        shelf INTEGER,
+        FOREIGN KEY (shelf) REFERENCES Shelf (id) ON DELETE CASCADE
+      )
+    ''');
+  }
+
+  Future<int> insert(String table, Map<String, dynamic> values) async {
+    final db = await database;
+    return await db.insert(table, values);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAll(String table) async {
+    final db = await database;
+    return await db.query(table);
+  }
+
+  Future<int> update(String table, Map<String, dynamic> values, int id) async {
+    final db = await database;
+    return await db.update(table, values, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> delete(String table, int id) async {
+    final db = await database;
+    return await db.delete(table, where: 'id = ?', whereArgs: [id]);
+  }
+}
