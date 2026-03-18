3bcf0e10051cb06b7b197966c0ef366a826a6f26
valooozz <valentin.corazza@ikmail.com>
2026-03-18 22:42:53 +0100

    migration bdd

--- lib/data/db/database_helper.dart
+++ lib/data/db/database_helper.dart
@@ -5,7 +5,6 @@ import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   static const _databaseName = "storage.db";
-  static const _databaseVersion = 1;
 
   static final DatabaseHelper instance = DatabaseHelper._internal();
   DatabaseHelper._internal();
@@ -18,74 +17,79 @@ class DatabaseHelper {
     return _database!;
   }
 
-  Future<Database> _initDatabase() async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, _databaseName);
+  Map<int, String> migrationScripts = {
+    1: '''CREATE TABLE House(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+          );
 
-    return openDatabase(path, version: _databaseVersion, onCreate: _onCreate);
-  }
+          CREATE TABLE Room(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            house INTEGER,
+            FOREIGN KEY (house) REFERENCES House (id) ON DELETE CASCADE
+          );
 
-  Future _onCreate(Database db, int version) async {
-    await db.execute('''
-      CREATE TABLE House(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL
-      )
-    ''');
+          CREATE TABLE Furniture(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            room INTEGER,
+            FOREIGN KEY (room) REFERENCES Room (id) ON DELETE CASCADE
+          );
 
-    await db.execute('''
-      CREATE TABLE Room(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        house INTEGER,
-        FOREIGN KEY (house) REFERENCES House (id) ON DELETE CASCADE
-      )
-    ''');
+          CREATE TABLE Shelf(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            furniture INTEGER,
+            FOREIGN KEY (furniture) REFERENCES Furniture (id) ON DELETE CASCADE
+          );
 
-    await db.execute('''
-      CREATE TABLE Furniture(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        room INTEGER,
-        FOREIGN KEY (room) REFERENCES Room (id) ON DELETE CASCADE
-      )
-    ''');
+          CREATE TABLE Item(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            shelf INTEGER,
+            FOREIGN KEY (shelf) REFERENCES Shelf (id) ON DELETE CASCADE
+          );
+              ''',
+    2: '''
+      ALTER TABLE Item ADD COLUMN taken INTEGER NOT NULL DEFAULT 0;
 
-    await db.execute('''
-      CREATE TABLE Shelf(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        furniture INTEGER,
-        FOREIGN KEY (furniture) REFERENCES Furniture (id) ON DELETE CASCADE
-      )
-    ''');
-
-    await db.execute('''
-      CREATE TABLE Item(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        shelf INTEGER,
-        FOREIGN KEY (shelf) REFERENCES Shelf (id) ON DELETE CASCADE
-      )
-    ''');
-
-    await db.execute('''
       CREATE TABLE Trip(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        selected INTEGER
-      )
-    ''');
+        selected INTEGER NOT NULL DEFAULT 0
+      );
 
-    await db.execute('''
       CREATE TABLE TripItem(
         tripId INTEGER NOT NULL,
         itemId INTEGER NOT NULL,
         PRIMARY KEY (tripId, itemId),
         FOREIGN KEY (tripId) REFERENCES Trip (id) ON DELETE CASCADE,
         FOREIGN KEY (itemId) REFERENCES Item (id) ON DELETE CASCADE
-      )
-    ''');
+      );
+    ''',
+  };
+
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, _databaseName);
+
+    int databaseVersion = migrationScripts.length;
+
+    return openDatabase(
+      path,
+      version: databaseVersion,
+      onCreate: (Database db, int version) async {
+        for (int i = 1; i <= databaseVersion; i++) {
+          await db.execute(migrationScripts[i]!);
+        }
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        for (int i = oldVersion + 1; i <= newVersion; i++) {
+          await db.execute(migrationScripts[i]!);
+        }
+      },
+    );
   }
 
   Future<int> insert(String table, Map<String, dynamic> values) async {
